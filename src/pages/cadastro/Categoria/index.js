717c7af0d0726b25570a8f01b35b9a3f5a11abc9
pablohls <pablo.henrique_s@hotmail.com>
2020-07-28 21:56:22 -0300

    Comit Final Aula 2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,8 +5,20 @@ import { Link } from "react-router-dom";
 function CadastroCategoria(){
     return (
     <PageDefault>
-      <h1>Cadastro de Categoria
-      </h1>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
       <Link to="/">
         Ir para Home
